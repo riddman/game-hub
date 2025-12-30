0a3a6e954bf583603482f2a3ec7edf75b6ecd554
o.polishchuk <o.polishchuk@ideil.com>
2025-12-30 12:10:27 +0200

    Build a responsive layout. Build NavBar. Implement dark mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
-import { Button, ButtonGroup } from '@chakra-ui/react';
+import { Button, ButtonGroup, Grid, GridItem, Show } from '@chakra-ui/react';
+import { NavBar } from './components/NavBar';
 
 
 function App() {
     return (
-        <Button colorScheme='blue'>Button</Button>
+        <Grid templateAreas={{
+            base: `"nav" "main"`,
+            lg: `"nav nav" "aside main"`
+        }}>
+            <GridItem area="nav">
+                <NavBar />
+            </GridItem>
+            <Show above="lg">
+                <GridItem area="aside">Aside</GridItem>
+            </Show>
+            <GridItem area="main">Main</GridItem>
+        </Grid>
     );
-
 }
 
 export default App;
---
+++ src/components/NavBar.tsx
@@ -0,0 +1,14 @@
+//rafce
+
+import { HStack, Image } from '@chakra-ui/react'
+import logo from '../assets/logo.webp';
+import ColorModeSwitch from './ColorModeSwitch';
+
+export const NavBar = () => {
+  return (
+    <HStack justifyContent={ 'space-between' } padding='10px'>
+        <Image src={ logo } boxSize="60px"/>
+        <ColorModeSwitch />
+    </HStack>
+  )
+}
