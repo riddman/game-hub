0c21bb17a0d65646cb6894af0edb4a8ddd5c1a5f
riddman <riddleman@ukr.net>
2026-02-08 23:00:58 +0200

    Add a dynamic page heading. Clean up the genres

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,4 +1,4 @@
-import { Button, HStack, Image, List, ListItem, Spinner, Text } from '@chakra-ui/react';
+import { Button, Heading, HStack, Image, List, ListItem, Spinner, Text } from '@chakra-ui/react';
 import useGenres, { Genre } from '../hooks/useGenres';
 import getCroppedImageUrl from '../services/image-url';
 
@@ -21,24 +21,30 @@ export default function GenreList(props: Props) {
     }
 
     return (
-        <List>
-            { data.map(genre => (
-                <ListItem key={ genre.id } paddingY="5px" >
-                    <HStack>
-                        <Image
-                            boxSize="32px"
-                            borderRadius={8}
-                            src={ getCroppedImageUrl(genre.image_background) }
-                        />
-                        <Button
-                            fontSize="lg"
-                            variant="link"
-                            fontWeight={ genre.id === props.selectedGenre?.id  ? 'bold' : 'normal' }
-                            onClick={ () => props.onSelectGenre(genre) }
-                        >{ genre.name }</Button>
-                    </HStack>
-                </ListItem>
-            ))}
-        </List>
+        <>
+            <Heading fontSize="2xl" marginBottom={ 3 }>Genres</Heading>
+            <List>
+                { data.map(genre => (
+                    <ListItem key={ genre.id } paddingY="5px" >
+                        <HStack>
+                            <Image
+                                boxSize="32px"
+                                borderRadius={8}
+                                objectFit="cover"
+                                src={ getCroppedImageUrl(genre.image_background) }
+                            />
+                            <Button
+                                fontSize="lg"
+                                variant="link"
+                                whiteSpace="normal"
+                                textAlign='left'
+                                fontWeight={ genre.id === props.selectedGenre?.id  ? 'bold' : 'normal' }
+                                onClick={ () => props.onSelectGenre(genre) }
+                            >{ genre.name }</Button>
+                        </HStack>
+                    </ListItem>
+                ))}
+            </List>
+        </>
     )
 }
